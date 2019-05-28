ca8c77e355e0dae995c779875668f6416e76f502
Mauro Aranda <maurooaranda@gmail.com>
2019-05-27 21:18:11 -0300

    Update comments.
    * ctune.el: Update comments with support for other major modes.

--- ctune.el
+++ ctune.el
@@ -29,23 +29,24 @@
 
 ;; ctune: A package to tune out CC Mode Noise Macros.
 
-;; When working with projects that extensively use C Macros, the C file you
-;; are editing can contain 'Noise Macros' (e.g., C Macros that define GCC
-;; attributes).  These 'Noise Macros' (see the CC Mode Manual for more info)
-;; can confuse CC Mode, causing bad indentation, fontification, etc.
+;; When working with projects that extensively use C-style Macros,
+;; the buffer you are editing can contain 'Noise Macros' (e.g., C Macros that
+;; define GCC attributes).  These 'Noise Macros' (see the CC Mode Manual for
+;; more info) can confuse CC Mode, causing bad indentation, fontification, etc.
 ;; This package is an attempt to make the addition and removal of these
 ;; entities somewhat more automated than what CC Mode offers.
 
 ;; At the time of writing this package, I know of three ways of handling the
 ;; issue.
-;; 1) Editing .dir-locals.el by hand: Find the Noise Macro that is bothering
-;; you, and add an entry for the c-mode key in the alist of .dir-locals.el.
-;; You might be working in a project that already contains such entry, so
-;; changing the .dir-locals.el is easy, although somewhat tedious.
+;; 1) Editing `dir-locals-file' by hand: Find the Noise Macro that is bothering
+;; you, and add an entry for the correspondent major mode key in the alist of
+;; `dir-locals-file'.  You might be working in a project that already contains
+;; such entry, so changing the `dir-locals-file' is easy, although somewhat
+;; tedious.
 
-;; 2) Adding the entries in the c-mode buffer: You notice the Noise Macro, it
-;; bothers you, and then you add the entry to the list, like this:
-;; (add-to-list 'c-noise-macro-names '("NOISE_MACRO_THAT_BOTHERS_ME"))
+;; 2) Adding the entries manually, while editing the buffer: You notice the
+;; Noise Macro, it bothers you, and then you add the entry to the list, like
+;; this: (add-to-list 'c-noise-macro-names '("NOISE_MACRO_THAT_BOTHERS_ME"))
 ;; This is OK, but perhaps you make a mistake and use `c-noise-macro-names'
 ;; when `c-noise-macro-with-parens-names' was needed, or it bothers you to
 ;; have to type all the Noise Macro name, or killing and yanking the name still
@@ -59,7 +60,8 @@
 ;; easy and straight-forward.
 
 ;; ctune tries to handle both 3 ways by providing an easy way to add the Noise
-;; Macro to the right variable and save the values in the .dir-locals.el file.
+;; Macro to the right variable and save the values in the `dir-locals-file'
+;; file.
 
 ;;; Suggestions/Ideas:
 
